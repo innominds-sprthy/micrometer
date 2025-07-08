28aa305527297107114f1125e54198a37cf51ab4
INNOMINDSHYD\sdesha <sdesha@innominds.com>
2025-07-08 12:56:38 +0530

    initial commit - micrometer demo app

---
+++ order-service/src/main/java/com/example/observability/order/controller/OrderController.java
@@ -0,0 +1,37 @@
+package com.example.observability.order.controller;
+
+import com.example.observability.order.model.dto.Order;
+import com.example.observability.order.service.OrderService;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/order")
+@Slf4j
+public class OrderController {
+
+	private final OrderService orderService;
+
+	public OrderController(OrderService orderService) {
+		this.orderService = orderService;
+	}
+
+	@PostMapping("/place-order")
+	public ResponseEntity<String> placeOrder(@RequestBody Order order) {
+		log.info("Received Order Input :: {}", order);
+		orderService.placeOrder(order);
+		return ResponseEntity.ok("Order has been placed");
+	}
+
+	@PostMapping("/update-order-status")
+	public ResponseEntity<String> updateOrderStatus(@RequestBody Order order) {
+		orderService.updateOrderStatus(order);
+		return ResponseEntity.ok("Order has been updated");
+	}
+}
